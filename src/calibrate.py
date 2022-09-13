fbec1257133c4479f9d0855b0ae0a34bcc439228
Kaia <kaia.j.williams@gmail.com>
2022-09-13 15:51:23 -0600

    started to add stuff from hdmidiffractivetest

--- src/calibrate.py
+++ src/calibrate.py
@@ -1,5 +1,9 @@
+from turtle import width
 from Thorlabs.TLPM import TLPM
 from tlpm_interface import *
+from meadowlark_tools.MeadowlarkInterface import SLM_Interface
+import numpy
+from typing import List
 
 
 def run_calibration() -> None:
@@ -18,5 +22,39 @@ def run_calibration() -> None:
     # If you are generating a global calibration (recommended) the number of regions is 1,
     # if you are generating a regional calibration (typically not necessary) the number of regions is 64
     numRegions: int = 1
+
+    slmInstance = SLM_Interface()
+
+    # this should all get functionalized, but for now
+    # is a bastard combination of my stuff and Meadowlark stuff
+
+    width = slmInstance.SLMwidth.value
+    height = slmInstance.SLMheight.value
+    depth = slmInstance.bytesPerPixel
+
+    try:
+        imVolume = width * height * depth
+    except NameError:
+        print("WARNING: slmInstance not defined")
+        imVolume = 50
+
+    # Create an array to hold the image data
+    imageData = numpy.empty([imVolume], dtype=numpy.unit8, order="C")
+
+    # Create a blank vector to hold the wavefront correction
+    WFC = numpy.empty([imVolume], dtype=numpy.uint8, order="C")
+
+    # create an array to hold the intensities recorded by the power meter
+    dataIntensities: List[float] = []
+
+    # start the SLM blank, we are using RGB images
+    isEightBit = False
+    RGB: bool = True
+    vert: bool = True
+
+    SLM_Interface.libImage.Generate_Solid(
+        imageData, WFC, slmInstance.SLMwidth.value, height, depth, 0, RGB
+    )
+    SLM_Interface.libImage.Write_image(imageData, isEightBit)
 
     return
