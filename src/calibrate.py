578ff4c6153411ad1e6e4fec18efe7d68ebbb910
Kaia <kaia.j.williams@gmail.com>
2022-09-13 15:35:31 -0600

    add meadowlark .dlls

--- src/calibrate.py
+++ src/calibrate.py
@@ -12,4 +12,11 @@ def run_calibration() -> None:
     measure_power_average(power_meter, measPointsToAverage=points_to_average)
     power_meter.close()
 
+    # The number of data points we will use in the calibration is 256 (8 bits)
+    numDataPoints: int = 256
+
+    # If you are generating a global calibration (recommended) the number of regions is 1,
+    # if you are generating a regional calibration (typically not necessary) the number of regions is 64
+    numRegions: int = 1
+
     return
